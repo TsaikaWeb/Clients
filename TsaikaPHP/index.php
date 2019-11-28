477cf2340721c9e5c7ca16d6d2d45c17af477ae9
sempiternus666 <50131158+sempiternus666@users.noreply.github.com>
2019-11-28 14:05:31 +0200

    Add files via upload

--- TsaikaPHP/index.php
+++ TsaikaPHP/index.php
@@ -1,5 +1,16 @@
 ﻿<?php
 $nimed = simplexml_load_file("inimene.xml");
+$form = "Mees";
+if(isset($_GET['x'])){
+    switch ($_GET['x']) {
+        case 'mehed':
+            $form = "Man";
+            break;
+        case 'naised':
+            $form = "Woman";
+    }
+}
+
 echo $nimed -> nimi[0] -> attributes() ->id;
 function searchComputersByName($query){
     global $nimed;
@@ -15,13 +26,15 @@ function searchInimesedBySugu($query){
     global $nimed;
     $result = array();
     foreach ($nimed -> nimi as $nimi1){
-        if (substr(strtolower($nimi1 -> sugu), 0, strlen($query))== strtolower($query))
+        if (substr(strtolower($nimi1 -> emakeelne), 0, strlen($query))== strtolower($query))
             array_push($result, $nimi1);
     }
     return $result;
 
 }
 ?>
+
+
 <!DOCTYPE html>
 
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -31,14 +44,14 @@ function searchInimesedBySugu($query){
     <meta charset="utf-8">
 </head>
 <body>
-<h1 class="text-center">Nimede Otsi</h1>
+<h1 class="text-center">Table of clients</h1>
 <div class="container">
     <div class="row justify-content-md-center">
 <table border="1" class="table table-dark table-hover">
     <tr>
-        <th width="12%">Sugu</th>
-        <th width="12%">Emakeelne</th>
-        <th width="12%">Võrkeelne</th>
+        <th width="12%">Gender</th>
+        <th width="12%">Name</th>
+        <th width="12%">Name in different language</th>
 
 
     </tr>
@@ -56,30 +69,30 @@ function searchInimesedBySugu($query){
     ?>
 </table>
 <div class="container text-center">
-    <h2>Esimene Funktsioon</h2>
+    <h2>First Function</h2>
 </div>
 <div class="container text-center bg-secondary text-white">
 
-<h3>Esimene nimi emakeelne</h3>
+<h3>Last name added in table</h3>
 <?php echo $nimed -> nimi[0] -> emakeelne; ?>
 
 </div>
 <div class="container text-center ">
 
-    <h2>Teine Funktsioon</h2>
+    <h2>Second Function</h2>
 
 <div class=" text-center bg-secondary text-white">
 
-<h3>Otsige sugu järgi: </h3>
+<h3>Search by name:</h3>
     <form action="?" method="post">
-        <input type="text" name="search" placeholder="Sugu"/>
+        <input type="text" name="search" placeholder="Name"/>
         <input type="submit" value="Leida" />
     </form>
     <table border="1" class="table table-dark table-hover">
         <tr>
-            <th>Sugu</th>
-            <th>Emakeelne</th>
-            <th>Vorkkeelne</th>
+            <th>Gender</th>
+            <th>Name</th>
+            <th>Name in different language</th>
 
 
         </tr>
@@ -102,14 +115,40 @@ function searchInimesedBySugu($query){
 
 </div>
 </div>
-<div class="container text-center">
-    <h2>Kolmas Funktsioon</h2>
+<div class="container">
+    <div class="row justify-content-md-center">
+
+        <a href="index.php?x=mehed"><h3>Display only men</h3></a>
+        <hr>
+        <a href="index.php?x=naised"><h3>Display only women</h3></a>
+
+
+
+        <table class="table table-dark table-hover">
+            <thead>
+            <tr>
+                <th scope="col">Gender</th>
+                <th scope="col">Name</th>
+                <th scope="col">Name in different language</th>
+
+
+            </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach($nimed->nimi as $nimi2) {
+                if($nimi2->sugu == $form){
+                    echo '<tr>';
+                    echo '<td>'.$nimi2->emakeelne.'</td>';
+                    echo '<td>'.$nimi2->vorkkeelne.'</td>';
+                    echo '<td>'.$nimi2->sugu.'</td>';
+                    echo '</tr>';
+                }
+            }
+            ?>
+            </tbody>
+        </table>
 </div>
-<div class="container text-center bg-secondary text-white">
-
-    <h3>Esimene nimi emakeelne</h3>
-    <?php echo $nimed -> nimi[0] -> emakeelne; ?>
-
 </div>
 </body>
 </html>
